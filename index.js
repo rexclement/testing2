13e990d113d05e04871117351c9e849a69019f23
rexclement <146126821+rexclement@users.noreply.github.com>
2025-05-11 00:01:01 +0530

    Update ind

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "https://rexclement.github.io/frontend/", // React frontend
+  origin: "https://rexclement.github.io/frontend", // React frontend
   credentials: true // Allow sending cookies
 }));
 app.use(express.json()); // Enables JSON body parsing
